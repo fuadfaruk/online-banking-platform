e55f0941dcad3684c316bf1470258584a8da2f0f
Fuad Hassan Faruk <fuadfaruk707@gmail.com>
2026-09-07 19:59:48 +0600

    Add RightSidebar component and integrate user details; enhance Home layout with recent transactions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'John' }
+  const loggedIn = { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com' }
   return (
     <section className="home">
       <div className="home-content">
@@ -21,7 +22,15 @@ const Home = () => {
           totalCurrentBalance={125}
         />
         </header >
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 6789.50 }]}
+      />
     </section>
   )
 }
