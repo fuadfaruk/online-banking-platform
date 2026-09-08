9d9bdff19d0081f74a1da1dc5b10903f48fb8891
Fuad Hassan Faruk <fuadfaruk707@gmail.com>
2026-09-08 13:58:50 +0600

    Enhance BankCard component with user details and icons; refactor RightSidebar layout; add MyBanks, Transfer, and TransactionHistory pages

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,4 +1,5 @@
 import { formatAmount } from '@/lib/utils'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,7 +7,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
   return (
     <div className="flex flex-col">
         <Link href="/" className="bank-card">
-          <div className="bank-card-content">
+          <div className="bank-card_content">
             <div>
               <h1 className="text-16 font-semibold text-white">
                 {account.name || userName}
@@ -15,8 +16,47 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                 {formatAmount(account.currentBalance)}
               </p>
             </div>
+            <div className="flex justify-between">
+              <article className="flex flex-col gap-2">
+                <h1 className="text-12 font-semibold text-white">
+                  {userName}
+                </h1>
+                <h2 className="text-12 font-semibold text-white">
+                ⬤⬤ / ⬤⬤
+                </h2>
+                <p className="text-14 font-semibold tracking-[1.1px] text-white">
+                    ⬤⬤⬤⬤ ⬤⬤⬤⬤ ⬤⬤⬤⬤ <span className="text-16">
+                    1234
+                  </span>
+                </p>
+              </article>
+            </div>
           </div>
+
+          <div className="bank-card_icon">
+            <Image
+              src="/icons/Paypass.svg"
+              width={20}
+              height={24}
+              alt="pay"
+            />
+            <Image
+              src="/icons/mastercard.svg"
+              width={45}
+              height={32}
+              alt="mastercard"
+              className="ml-5"
+            />
+          </div>
+          <Image
+            src="/icons/lines.png"
+            width={316}
+            height={190}
+            alt="lines"
+            className="absolute top-0 left-0"
+          />
         </Link>
+      {}
     </div>
   )
 }
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -7,21 +7,20 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
     <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
-        <div className="profile-banner">
-          <div className="profile">
-            <div className="profile-img">
-              <span className="text-5xl font-bold text-blue-500">
-                { user.firstName[0] }
-              </span>
-            </div>
-            <div className="profile-details">
-              <h1 className='profile-name'>
-                { user.firstName } { user.lastName }
-              </h1>
-              <p className="profile-email">
-                { user.email }
-              </p>
-            </div>
+        <div className="profile-banner" />
+        <div className="profile">
+          <div className="profile-img">
+            <span className="text-5xl font-bold text-blue-500">
+              { user.firstName[0] }
+            </span>
+          </div>
+          <div className="profile-details">
+            <h1 className='profile-name'>
+              { user.firstName } { user.lastName }
+            </h1>
+            <p className="profile-email">
+              { user.email }
+            </p>
           </div>
         </div>
       </section>
@@ -43,8 +42,8 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         </div>
 
         {banks?.length > 0 && (
-          <div className="relative flex flex-1 flex-col intems-center justify-center gap-5">
-            <div className="realtive z-10">
+          <div className="relative flex flex-1 flex-col items-center justify-center gap-5">
+            <div className="relative z-10">
               <BankCard 
                 key={banks[0].$id}
                 account={banks[0]}
